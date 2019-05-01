c8654bc0ae0e2bfcd5d01afd9e7703ec92e4f760
Gabriel Romon <gabriel.romon@ensae.fr>
2019-05-01 19:05:44 +0200

    updated (Matjax retired their CDN years ago)

--- script.user.js
+++ script.user.js
@@ -9,13 +9,11 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
-
-     if (window.MathJax === undefined) {
+javascript: (function() {
+    if (window.MathJax === undefined) {
         var script = document.createElement("script");
         script.type = "text/javascript";
-        script.src = "https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_HTML";
+        script.src = "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-AMS_HTML";
         var config = 'MathJax.Hub.Config({' + 'extensions: ["tex2jax.js"],' + 'tex2jax: { inlineMath: [["$","$"],["\\\\\\\\\\\\(","\\\\\\\\\\\\)"]], displayMath: [["$$","$$"],["\\\\[","\\\\]"]], processEscapes: true },' + 'jax: ["input/TeX","output/HTML-CSS"]' + '});' + 'MathJax.Hub.Startup.onload();';
         if (window.opera) {
             script.innerHTML = config
@@ -23,10 +21,6 @@
             script.text = config
         }
         document.getElementsByTagName("head")[0].appendChild(script);
-        (doChatJax = function() {
-            window.setTimeout(doChatJax, 1000);
-            MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
-        })();
     } else {
         MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
     }
